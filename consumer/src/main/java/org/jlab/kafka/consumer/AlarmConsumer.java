da15a733de05510c1e7a969a00541e276ae9cfe2
ryans <ryans@jlab.org>
2020-03-10 13:43:19 -0400

    Use environment variable for Kafka URL

--- consumer/src/main/java/org/jlab/kafka/consumer/AlarmConsumer.java
+++ consumer/src/main/java/org/jlab/kafka/consumer/AlarmConsumer.java
@@ -4,21 +4,28 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
 public class AlarmConsumer {
-    public void start() {
+    public void start() throws IOException {
+        String url = System.getenv("KAFKA_URL");
+
+        if(url == null) {
+            throw new IOException("Environment variable KAFKA_URL not found");
+        }
+
         Properties props = new Properties();
-        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("bootstrap.servers", url);
         props.setProperty("group.id", "test");
         props.setProperty("enable.auto.commit", "true");
         props.setProperty("auto.commit.interval.ms", "1000");
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("foo", "bar"));
+        consumer.subscribe(Arrays.asList("alarms"));
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
             for (ConsumerRecord<String, String> record : records)
@@ -26,7 +33,7 @@ public class AlarmConsumer {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AlarmConsumer consumer = new AlarmConsumer();
         consumer.start();
     }
--- producer/src/main/java/org/jlab/kafka/producer/AlarmGenerator.java
+++ producer/src/main/java/org/jlab/kafka/producer/AlarmGenerator.java
@@ -4,13 +4,19 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class AlarmGenerator {
-    public void start() {
+    public void start() throws IOException {
+        String url = System.getenv("KAFKA_URL");
+
+        if(url == null) {
+            throw new IOException("Environment variable KAFKA_URL not found");
+        }
 
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
+        props.put("bootstrap.servers", url);
         props.put("acks", "all");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
@@ -21,7 +27,7 @@ public class AlarmGenerator {
 
         producer.close();
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AlarmGenerator generator = new AlarmGenerator();
 
         generator.start();
