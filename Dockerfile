78f0040d468518bf9e8b4fe0d47fe486da2366aa
ryans <ryans@jlab.org>
2020-07-16 10:18:35 -0400

    re-organizing project

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,9 @@ RUN git clone https://github.com/JeffersonLab/kafka-streams-epics-alarms \
     && gradle build -x test \
     && cp -r ./build/install/* /opt \
     && cp ./docker-entrypoint.sh / \
-    && chmod +x /docker-entrypoint.sh
+    && chmod +x /docker-entrypoint.sh \
+    && rm -rf /home/gradle/kafka-streams-epics-alarms
+
+WORKDIR /
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- src/main/java/org/jlab/kafka/streams/CAAggregate.java
+++ src/main/java/org/jlab/kafka/streams/CAAggregate.java
@@ -18,9 +18,14 @@ public final class CAAggregate {
     public static final String OUTPUT_TOPIC = "active-alarms";
 
     static Properties getStreamsConfig() {
+
+        String bootstrapServers = System.getenv("BOOTSTRAP_SERVERS");
+
+        bootstrapServers = (bootstrapServers == null) ? "localhost:9092" : bootstrapServers;
+
         final Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-ca-aggregate");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafkatest:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
