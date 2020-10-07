f00a11016110d48a8a1a759743f7a9748b5809cb
ryans <ryans@jlab.org>
2020-10-07 13:26:12 -0400

    Configuring AVRO support

--- src/main/java/org/jlab/kafka/streams/AutoConfigureConnectEpics.java
+++ src/main/java/org/jlab/kafka/streams/AutoConfigureConnectEpics.java
@@ -13,13 +13,15 @@ import java.util.concurrent.CountDownLatch;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
+
 public final class AutoConfigureConnectEpics {
 
     private static final Logger LOGGER = Logger.getLogger("org.jlab.kafka.streams.CAAgrregate");
 
-    public static final String INPUT_TOPIC1 = "channel1";
-    public static final String INPUT_TOPIC2 = "channel2";
-    public static final String OUTPUT_TOPIC = "active-alarms";
+    // TODO: these need to be configurable
+    public static final String INPUT_TOPIC = "registered-alarms";
+    public static final String OUTPUT_TOPIC = "epics-channels";
 
     static Properties getStreamsConfig() {
 
@@ -27,35 +29,32 @@ public final class AutoConfigureConnectEpics {
 
         bootstrapServers = (bootstrapServers == null) ? "localhost:9092" : bootstrapServers;
 
+        String registry = System.getenv("SCHEMA_REGISTRY");
+
+        registry = (registry == null) ? "http://localhost:8081" : registry;
+
         final Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-ca-aggregate");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "auto-configure-connect-epics");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(SCHEMA_REGISTRY_URL_CONFIG, registry);
 
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return props;
     }
 
     static void createRuleStream(final StreamsBuilder builder) {
-        KStream<String, String> source1 = builder.stream(INPUT_TOPIC1);
-        source1 = source1.selectKey((k,v) -> "channel1");
+        KStream<String, String> input = builder.stream(INPUT_TOPIC);
 
-        KStream<String, String> source2 = builder.stream(INPUT_TOPIC2);
-        source2 = source2.selectKey((k,v) -> "channel2");
-
-        final KStream<String, String> source = source1.merge(source2);
-
-        boolean changed = true; // TODO: We must store previous value of SEVR field and compare with new value!
-
-        final KStream<String, String> alarms = source.filter((k, v) -> {
+        final KStream<String, String> output = input.filter((k, v) -> {
             System.out.printf("key = %s, value = %s%n", k, v);
 
-            return changed;
+            return v.contains("epics2kafka");
         });
 
-        alarms.to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+        output.to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
     }
 
     public static void main(final String[] args) {
@@ -67,7 +66,7 @@ public final class AutoConfigureConnectEpics {
         final CountDownLatch latch = new CountDownLatch(1);
 
         // attach shutdown handler to catch control-c
-        Runtime.getRuntime().addShutdownHook(new Thread("streams-ca-aggregate-shutdown-hook") {
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
                 streams.close();
