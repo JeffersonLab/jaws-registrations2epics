900a94becc1f77a43cc0ed6a1edcb1698658f21c
ryans <ryans@jlab.org>
2024-07-08 12:53:08 -0400

    Use Google Java format

--- src/test/java/org/jlab/jaws/Registrations2EpicsTest.java
+++ src/test/java/org/jlab/jaws/Registrations2EpicsTest.java
@@ -1,5 +1,9 @@
 package org.jlab.jaws;
 
+import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
+
+import java.util.Arrays;
+import java.util.Properties;
 import org.apache.kafka.streams.*;
 import org.jlab.jaws.entity.*;
 import org.junit.After;
@@ -7,64 +11,67 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Properties;
-
-import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
-
 public class Registrations2EpicsTest {
-    private TopologyTestDriver testDriver;
-    private TestInputTopic<String, AlarmInstance> inputTopic;
-    private TestOutputTopic<String, String> outputTopic;
-    private AlarmInstance instance = new AlarmInstance();
+  private TopologyTestDriver testDriver;
+  private TestInputTopic<String, AlarmInstance> inputTopic;
+  private TestOutputTopic<String, String> outputTopic;
+  private AlarmInstance instance = new AlarmInstance();
 
-    @Before
-    public void setup() {
-        final Properties streamsConfig = Registrations2Epics.getStreamsConfig();
-        streamsConfig.put(SCHEMA_REGISTRY_URL_CONFIG, "mock://testing");
-        final Topology top = Registrations2Epics.createTopology(streamsConfig);
-        testDriver = new TopologyTestDriver(top, streamsConfig);
+  @Before
+  public void setup() {
+    final Properties streamsConfig = Registrations2Epics.getStreamsConfig();
+    streamsConfig.put(SCHEMA_REGISTRY_URL_CONFIG, "mock://testing");
+    final Topology top = Registrations2Epics.createTopology(streamsConfig);
+    testDriver = new TopologyTestDriver(top, streamsConfig);
 
-        // setup test topics
-        inputTopic = testDriver.createInputTopic(Registrations2Epics.INPUT_TOPIC, Registrations2Epics.INPUT_KEY_SERDE.serializer(), Registrations2Epics.INPUT_VALUE_SERDE.serializer());
-        outputTopic = testDriver.createOutputTopic(Registrations2Epics.OUTPUT_TOPIC, Registrations2Epics.OUTPUT_KEY_SERDE.deserializer(), Registrations2Epics.OUTPUT_VALUE_SERDE.deserializer());
+    // setup test topics
+    inputTopic =
+        testDriver.createInputTopic(
+            Registrations2Epics.INPUT_TOPIC,
+            Registrations2Epics.INPUT_KEY_SERDE.serializer(),
+            Registrations2Epics.INPUT_VALUE_SERDE.serializer());
+    outputTopic =
+        testDriver.createOutputTopic(
+            Registrations2Epics.OUTPUT_TOPIC,
+            Registrations2Epics.OUTPUT_KEY_SERDE.deserializer(),
+            Registrations2Epics.OUTPUT_VALUE_SERDE.deserializer());
 
-        EPICSSource source = new EPICSSource();
-        source.setPv("channel1");
+    EPICSSource source = new EPICSSource();
+    source.setPv("channel1");
 
-        instance.setSource(source);
-        instance.setAlarmclass("base");
-        instance.setLocation(Arrays.asList("INJ"));
-        instance.setScreencommand("/");
+    instance.setSource(source);
+    instance.setAlarmclass("base");
+    instance.setLocation(Arrays.asList("INJ"));
+    instance.setScreencommand("/");
+  }
+
+  @After
+  public void tearDown() {
+    if (testDriver != null) {
+      testDriver.close();
     }
+  }
 
-    @After
-    public void tearDown() {
-        if(testDriver != null) {
-            testDriver.close();
-        }
-    }
+  @Test
+  public void matchedTombstoneMsg() {
+    inputTopic.pipeInput("alarm1", instance);
+    inputTopic.pipeInput("alarm1", null);
+    KeyValue<String, String> result = outputTopic.readKeyValuesToList().get(1);
+    Assert.assertNull(result.value);
+  }
 
-    @Test
-    public void matchedTombstoneMsg() {
-        inputTopic.pipeInput("alarm1", instance);
-        inputTopic.pipeInput("alarm1", null);
-        KeyValue<String, String> result = outputTopic.readKeyValuesToList().get(1);
-        Assert.assertNull(result.value);
-    }
+  @Test
+  public void unmatchedTombstoneMsg() {
+    inputTopic.pipeInput("alarm1", null);
+    Assert.assertTrue(
+        outputTopic.isEmpty()); // Cannot transform a tombstone without a prior registration!
+  }
 
-
-    @Test
-    public void unmatchedTombstoneMsg() {
-        inputTopic.pipeInput("alarm1", null);
-        Assert.assertTrue(outputTopic.isEmpty()); // Cannot transform a tombstone without a prior registration!
-    }
-
-    @Test
-    public void regularMsg() {
-        inputTopic.pipeInput("alarm1", instance);
-        KeyValue<String, String> result = outputTopic.readKeyValue();
-        Assert.assertEquals("{\"topic\":\"alarm-activations\",\"channel\":\"channel1\"}", result.key);
-        Assert.assertEquals("{\"mask\":\"a\",\"outkey\":\"alarm1\"}", result.value);
-    }
+  @Test
+  public void regularMsg() {
+    inputTopic.pipeInput("alarm1", instance);
+    KeyValue<String, String> result = outputTopic.readKeyValue();
+    Assert.assertEquals("{\"topic\":\"alarm-activations\",\"channel\":\"channel1\"}", result.key);
+    Assert.assertEquals("{\"mask\":\"a\",\"outkey\":\"alarm1\"}", result.value);
+  }
 }
